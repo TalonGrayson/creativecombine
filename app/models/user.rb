da56b5739c77fc6d60c2ec40b782a20a29108927
Intuita-Stefan <stefan.ritchie@intuitaconsulting.com>
2018-02-22 14:50:11 +0000

    Figuring out the auth data

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@ class User < ApplicationRecord
     puts '-------------------------------------'
     puts "UID: " + auth.uid.to_s
     puts "Email: " + auth.info.email.to_s
-    puts "Display Name: " + auth.raw_info.display_name.to_s
-    puts "Logo: " + auth.raw_info.logo.to_s
+    puts "Display Name: " + auth.info.name.to_s
+    puts "Logo: " + auth.info.image.to_s
     puts "Token: " + auth.uid.to_s
     puts '-------------------------------------'
     where(uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = auth.raw_info.display_name
-      user.image_url = auth.raw_info.logo
+      user.username = auth.info.name
+      user.image_url = auth.info.image
       user.token = auth.credentials.token
       user.data['omniauth_info'] = auth
     end
