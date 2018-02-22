d8c8da35fd66ba5d2c1f69e561ab8d1529edd915
Intuita-Stefan <stefan.ritchie@intuitaconsulting.com>
2018-02-22 12:35:35 +0000

    Thirdly attempt at Twitch login

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,11 @@ Rails.application.routes.draw do
       unlocks: 'users/unlocks',
       omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
+  #devise_scope :user do
+  #  get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
+  #  get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  #end
+
   root to: 'pages#index'
 end
