095117676060a77f64b733e1f72e167b63205040
Intuita-Stefan <stefan.ritchie@intuitaconsulting.com>
2018-02-22 14:28:01 +0000

    SSL bullshit still getting in the way...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
   def index
+    puts "Index showing"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: %i[twitch]
+
+
+
+  def self.from_omniauth(auth)
+    where(uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.nickname
+      user.image_url = auth.info.image
+      user.token = auth.credentials.token
+      user.data['omniauth_info'] = auth
+    end
+  end
+
 end
