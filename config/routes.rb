e0a10f805028bfc6f6e9853a9543a73ebdf2c4a5
Intuita-Stefan <stefan.ritchie@intuitaconsulting.com>
2018-02-22 11:42:29 +0000

    Updated index, added stylesheet and favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      confirmations: 'users/confirmations',
+      mailer: 'users/mailer',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
+      shared: 'users/shared',
+      unlocks: 'users/unlocks'
+  }
   root to: 'pages#index'
 end
