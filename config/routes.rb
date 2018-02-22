e4dbbfe300cf9d768333c6c883563e7c28fba43c
Intuita-Stefan <stefan.ritchie@intuitaconsulting.com>
2018-02-22 16:22:26 +0000

    Adding Registered Users list

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-      #confirmations: 'users/confirmations',
-      #mailer: 'users/mailer',
-      #passwords: 'users/passwords',
-      #registrations: 'users/registrations',
-      #shared: 'users/shared',
-      #unlocks: 'users/unlocks',
       sessions: 'users/sessions',
       omniauth_callbacks: 'users/omniauth_callbacks'
   }
@@ -22,5 +16,6 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'users/sessions#destroy'
   end
 
-  get 'pages#registered_users'
+  get 'registered_users' => 'pages#registered_users'
+
 end
